b8c32e412fe5347f047db37288171ab41b891d71
bruno coelho <brunocoelho@luizalabs.com>
2023-05-29 09:20:28 -0300

    pretica

---
+++ python/python_iniciante/print.py
@@ -0,0 +1,36 @@
+# Estilos de print - Aula 1
+print('')
+print(2+2) # operação
+print('')
+print('ola mundo"') # texto
+print('')
+print('''
+
+ola mundo com quebra de linha sem crise.
+
+Oba!
+
+''') # texto com formatação livre
+print('')
+
+idade = 10
+
+print(f"eu tenho {idade} de idade!") # testo com uso de recurso de variavel
+
+# Estilos de print - Aula 2 
+
+print('Bruno '*5)
+print()
+
+# trabalhando com concatenação 
+
+nome = "Bruno "
+sobrenome = "Coelho"
+idade1= 27
+
+print(nome)
+print(sobrenome)
+
+print(nome+sobrenome)
+print('')
+print(nome+sobrenome+idade1) # Error não é possivel concatenar string com inteiro
---
+++ python/python_iniciante/variaveis.py
@@ -0,0 +1,46 @@
+'''
+tipode de variaveis 
+
+int = inteiro "10" 
+float = decimal "12,5"
+str = caracteres "abcd1234*!@"
+boolean = logico "True or false
+
+
+Nomear variaveis 
+
+criar nomes de var iniciando com letra minuscula
+começar com letra e nao com numero
+não deve conter espaço 
+
+Comando type mostra o tipo de variavel
+
+
+'''
+
+print('')
+
+nome = 'bruno'
+print(nome)
+
+nomedapessoa = 'silva'
+print(nomedapessoa)
+
+nome_da_pessoa ='caio'
+print(nome_da_pessoa)
+
+numero=10  
+print(type(numero)) 
+print(type(nome)) 
+
+
+nome1 = 'CARLOS'
+# maiuscula upper
+maiusculo= nome.upper()
+print(maiusculo)
+# 1* maiuscula capitalize 
+primeiramaiuscula= nome.capitalize()
+print(primeiramaiuscula)
+# minuscula lower
+minusculo = nome1.lower()
+print(minusculo)
